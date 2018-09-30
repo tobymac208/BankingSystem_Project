ca2e1fe8050ad7420137283c70244cf50de6467c
Nik <ironman_fern@comcast.net>
2018-09-29 21:05:41 -0500

    Make modifications to class

--- src/com/groupproject/group/Account/LoginAccount/ManagerAccount.java
+++ src/com/groupproject/group/Account/LoginAccount/ManagerAccount.java
@@ -1,8 +1,13 @@
 package com.groupproject.group.Account.LoginAccount;
 
 public class ManagerAccount extends LoginAccount {
+    // TODO: Implement LinkedList functionality for holding the user accounts
+
     public ManagerAccount(String firstName, String lastName, int age){
         // call the base class constructor, passing in all arguments
         super(firstName, lastName, age);
     }
+
+    // TODO: 1. Add functionality for adding a user
+    // TODO: 2. Add functionality for removing a user
 }
