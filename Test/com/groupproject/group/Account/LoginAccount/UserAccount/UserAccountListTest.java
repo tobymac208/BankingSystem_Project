f2e7fbd84a195f362b072d5a80d721f1ca04dd13
Nik <ironman_fern@comcast.net>
2018-11-20 16:50:35 -0600

    Added Test directory and a template for UserAccountListTest class

---
+++ Test/com/groupproject/group/Account/LoginAccount/UserAccount/UserAccountListTest.java
@@ -0,0 +1,68 @@
+package com.groupproject.group.Account.LoginAccount.UserAccount;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Description: Test class for UserAccountList. Template.
+ */
+
+class UserAccountListTest {
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getUsers1() {
+    }
+
+    @Test
+    void add1() {
+    }
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void addMany() {
+    }
+
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void verifyPassword() {
+    }
+
+    @Test
+    void countOccurrences() {
+    }
+
+    @Test
+    void ensureCapacity() {
+    }
+
+    @Test
+    void getCapacity() {
+    }
+
+    @Test
+    void removeAccount() {
+    }
+
+    @Test
+    void getSize() {
+    }
+}
